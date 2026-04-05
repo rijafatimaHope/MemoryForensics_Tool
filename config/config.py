5a1008aff5fd76bba8657f1503acbc076818973d
= <muhammadfaraz800@gmail.com>
2026-04-05 08:40:20 +0500

    feat(phase-1): Setup Configuration & Project Structure

---
+++ config/config.py
@@ -0,0 +1,11 @@
+# config.py
+# Constants and configurations for MemoryForensics Tool
+
+# The maximum percentage of system RAM we are allowed to use when chunking
+MEMORY_LIMIT_PCT = 0.40
+
+# Logging verbosity
+LOG_LEVEL = "INFO"
+
+# Default chunk sizes for reading (in bytes) if we fall back from mmap
+DEFAULT_CHUNK_SIZE = 10 * 1024 * 1024  # 10 MB chunks
